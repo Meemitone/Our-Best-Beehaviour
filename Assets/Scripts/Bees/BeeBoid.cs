0bbec7808d375146e4e8b1984d5548904fa0c68d
DeeAich <114917172+DeeeAich@users.noreply.github.com>
2024-02-29 14:34:29 +0000

    Bees structure made

---
+++ Assets/Scripts/Bees/BeeBoid.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using UnityEngine;
+using System;
+
+namespace Assets.Scripts.Bees
+{
+    public class BeeBoid : BoidBasic
+    {
+
+
+
+    }
+
+    public class Wander : Behaviour
+    {
+
+    }
+
+    public class Flock : Behaviour
+    {
+
+    }
+
+    public class Flee : Behaviour
+    {
+
+    }
+
+    public class Seek : Behaviour
+    {
+
+    }
+
+    public class Arrive : Behaviour
+    {
+
+    }
+
+    public class Collect : Behaviour
+    {
+
+    }
+
+    public class Pursue : Behaviour
+    {
+
+    }
+}
