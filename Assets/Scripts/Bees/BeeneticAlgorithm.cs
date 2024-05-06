423d2dd45a613ff4f1c15f0c150507e38861b572
Mixer-one <99894586+Mixer-one@users.noreply.github.com>
2024-05-06 18:36:06 +0100

    bee sex

--- Assets/Scripts/Bees/BeeneticAlgorithm.cs
+++ Assets/Scripts/Bees/BeeneticAlgorithm.cs
@@ -5,16 +5,26 @@ using UnityEngine;
 public class BeeneticAlgorithm : MonoBehaviour
 {
     public float[] Benes = new float[5];
+    public bool NewBee;
     
     
     // Start is called before the first frame update
-    void Awake()
+    void Start()
     {
-        for (int i = 0; i < Benes.Length; i++)
-        {
-            Benes[i] = Random.Range(0.0f, 1.0f);
-        }
+        LateStart();
     }
 
+    public IEnumerator LateStart()
+    {
+        yield return new WaitForSeconds(0.01f);
+        if (!NewBee)
+        {
+            for (int i = 0; i < Benes.Length; i++)
+            {
+                Benes[i] = Random.Range(0.0f, 1.0f);
+            }
+        }
+            
+    }
 
 }
--- Assets/Scripts/Hive/HiveGeneticsManager.cs
+++ Assets/Scripts/Hive/HiveGeneticsManager.cs
@@ -6,39 +6,30 @@ using UnityEngine;
 
 public class HiveGeneticsManager : MonoBehaviour
 {
- //extra stats cause i cant think :(
+    private int Beenome;
+    private int ParentA;
+    private int ParentB;
     private int BeeNumber;
+    private int BeesToMake;
     public float[] Benetics = new float[5];
+    public float[] Baybee = new float[5];
     public GameObject[] Bees;
     [SerializeField] private List<BeeneticAlgorithm> BA = new List<BeeneticAlgorithm>();
     private int Beemoved;
     private GameObject Beefab;
-    private void Awake()
+    [SerializeField] private Vector3 Spawnpoint;
+
+private void Awake()
     {
         BeeNumber = 0;
     }
     
-    /*void Start()
+    void Start()
     {
-        
-        for (int i = 0; i < 10; i++)
-        {
-            BeeNumber = i;
 
-@@ -31,39 +31,26 @@ public class HiveGeneticsManager : MonoBehaviour
-                Benetics[BeeNumber, j] = BA.Benes[j];
-                //Debug.Log(Benetics[BeeNumber, i]);
-            }
-            
-        }
+    }
 
-
-    
-    
-    
-    }*/
-
-    private void OnTriggerEnter(Collider other) //will correct depending on how we decide to trigger a bee entering the hive. Adds a bees genes to the hive.
+    private void OnTriggerEnter(Collider other) //will correct depending on how we decide to trigger a bee entering the hive. Adds a bees genes to the hive when they enter.
     {
         if (other.tag == "Bee")
         {
@@ -51,13 +42,71 @@ public class HiveGeneticsManager : MonoBehaviour
         
     }
     
-
-
-    public void OnBeeLeaving()
+    public void OnBeeLeaving(int Beeleavers) // call OnBeeLeaving(int) to spawn a bees in the hive equal to the chosen number or until hive is empty
     {
-       Beemoved  = Random.Range(0,BeeNumber);
-       Beefab.GetComponent<BeeneticAlgorithm>().Benes = BA.ElementAt(Beemoved).Benes;
-       BA.RemoveAt(Beemoved);
-       BeeNumber--;
+        for (int i = 0; i < Beeleavers; i++)
+        {
+            if (BeeNumber > 0)
+            {
+                Beemoved = Random.Range(0, BeeNumber);
+                Beefab.GetComponent<BeeneticAlgorithm>().Benes = BA.ElementAt(Beemoved).Benes;  // expected trouble
+                Beefab.GetComponent<BeeneticAlgorithm>().NewBee = true;
+                Instantiate(Beefab, Spawnpoint, Quaternion.identity);
+                BA.RemoveAt(Beemoved);
+                BeeNumber--;
+            }
+            else
+            {
+                break;
+            }
+        }
     }
+
+
+    public void Beeproduction(int NewBees)
+    {
+        BeesToMake = NewBees;
+        for (int x = 0; x < NewBees; x++)
+        {
+            ParentA = Random.Range(0, BeeNumber);
+            ParentB = Random.Range(0, BeeNumber);
+            if (ParentB == ParentA)
+            {
+                Beeproduction(BeesToMake);
+            }
+            else
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    Beenome = Random.Range(0, 2);
+                    if (Beenome == 1)
+                    {
+                        Baybee[i] = BA.ElementAt(ParentA).Benes[i] + Random.Range(-0.01f, 0.01f);
+                    }
+                    else if (Beenome != 1)
+                    {
+                        Baybee[i] = BA.ElementAt(ParentB).Benes[i] + Random.Range(-0.01f, 0.01f);
+                    }
+
+                    if (i == 5)
+                    {
+                        this.GameObject().AddComponent<BeeneticAlgorithm>();
+                        for (int j = 0; j < 5; j++)
+                        {
+                            this.GameObject().GetComponent<BeeneticAlgorithm>().Benes[j] = Baybee[j];
+                            if (j == 5)
+                            {
+                                BA.Add(this.GameObject().GetComponent<BeeneticAlgorithm>());
+                                Destroy(this.GameObject().GetComponent<BeeneticAlgorithm>());
+                            }
+                        }
+
+                        BeeNumber++;
+                        BeesToMake--;
+                    }
+                }
+            }
+        }
+    }
+    
 }
